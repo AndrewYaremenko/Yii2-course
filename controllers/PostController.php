1b8340988d7ae9954be1d3972c8c6c0397ef07e9
Andrew Yaremenko <esdesuagk@gmail.com>
2023-07-24 13:57:38 +0300

    views, patterns

--- controllers/PostController.php
+++ controllers/PostController.php
@@ -3,17 +3,20 @@
 namespace app\controllers;
 
 use app\controllers\AppController;
-use Yii;
 
 class PostController extends AppController
 {
+
+    public $layout = 'basic';
+
     public function actionIndex()
     {
-
-        $names = ['Jhon', 'Bob', 'Andrew'];
-
-        debug([123,23,true]);
-
         return $this->render('index');
+    }
+
+    public function actionShow()
+    {
+        $this-> layout = 'main';
+        return $this->render('show');
     }
 }
--- views/post/show.php
+++ views/post/show.php
@@ -2,11 +2,9 @@
 /** @var yii\web\View $this */
 
 ?>
-<h1>post/index</h1>
+<h1>post/show</h1>
 
 <p>
     You may change the content of this page by modifying
     the file <code><?= __FILE__; ?></code>.
 </p>
-
-
